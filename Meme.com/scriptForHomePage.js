9099a19420ca30eff3b1b2bcd1d475ae0ac3970e
unknown <ivana_toneva@abv.bg>
2020-05-13 22:13:21 +0300

    removed

--- Meme.com/scriptForHomePage.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("Login").onclick = function()
-{
-    window.location.href="loginPage.html";
-}
-document.getElementById("Register").onclick = function()
-{
-    window.location.href="registerPage.html";
-}
